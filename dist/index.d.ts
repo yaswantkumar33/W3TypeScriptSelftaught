2d3fcf0c6458f8150ca0e93b10befa9fe326b9d1
articstark <yaswantkumar1512@gmail.com>
2025-08-10 21:53:01 +0530

    learned till tuples

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,12 @@
+declare let username: string;
+declare let userage: number;
+declare let isActive: boolean;
+declare let uservalue: any;
+declare let w: unknown;
+declare let y: undefined;
+declare let z: null;
+declare const names: string[];
+declare const names2: readonly string[];
+declare const testarray1: string[];
+declare let tuplearr1: [string, number, boolean, string];
 //# sourceMappingURL=index.d.ts.map
--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,53 @@
 "use strict";
-console.log("Hello world!");
+console.log("Welcome to the typescript learning!");
+// Type Script simple type values ley
+let username = "UserName";
+let userage = 26;
+let isActive = true;
+// Special types in type script 
+// Any Type 
+let uservalue = 54;
+uservalue = "Stark";
+uservalue = true;
+console.log(uservalue, "this is the example of the any type!/n");
+// Type: unknown
+// unknown is a similar, but safer alternative to any.
+// TypeScript will prevent unknown types from being used, as shown in the below example:
+let w = 1;
+w = "string"; // no error
+w = {
+    runANonExistentMethod: () => {
+        console.log("I think therefore I am");
+    }
+};
+// How can we avoid the error for the code commented out below when we don't know the type?
+// w.runANonExistentMethod(); // Error: Object is of type 'unknown'.
+if (typeof w === 'object' && w !== null) {
+    w.runANonExistentMethod();
+}
+// Although we have to cast multiple times we can do a check in the if to secure our type and have a safer casting
+// ---------------------------------
+// Type: undefined & null
+// undefined and null are types that refer to the JavaScript primitives undefined and null respectively.
+let y = undefined;
+let z = null;
+// Arrays in typescript 
+const names = []; //here we are assigning the array of string similiar to :number[] and boolean[]
+names.push('Name1');
+console.log(names);
+// readonly type 
+const names2 = ['name1', "name2"];
+// names2.push("name3") Error 
+// here in the above array it has read only which meas we can't push or edit any vales in that array 
+// Type Intefrence
+// if we have the array of string and we push the a different type in it means it shows error 
+const testarray1 = ["string1", "string2", 'string3'];
+// testarray1.push(2) Error
+// Tuples 
+// a tuple is a typed array with the fixed length and it's types for each index 
+// To define a tuple we must first give the tupes and length of the tuple 
+let tuplearr1;
+// initialize correctly 
+tuplearr1 = ["string", 25, true, "string"];
+console.log(tuplearr1);
 //# sourceMappingURL=index.js.map
