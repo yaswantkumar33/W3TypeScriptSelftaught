9df555da27a92950cb4048d5759a57a71580abb8
articstark <yaswantkumar1512@gmail.com>
2025-08-10 17:03:00 +0530

    initial commit before starting

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello world!");
+console.log("Welcome to the typescript learning!");
+// Type Script simple type values ley
+let username : string ="UserName"
+let userage : number =26
+let isActive : boolean =true
+// Special types in type script 
+// Any Type 
+let uservalue:any = 54;
+uservalue = "Stark";
+uservalue = true;
+console.log(uservalue,"this is the example of the any type!/n")
